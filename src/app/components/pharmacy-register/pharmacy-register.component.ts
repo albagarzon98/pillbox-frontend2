f349495522af6c56082845d3c662ae35b464adaa
Levian <levianlastra@gmail.com>
2021-09-03 20:26:59 -0300

    Finalizados los campos con sus validaciones, del form para registrar farmacia

--- src/app/components/pharmacy-register/pharmacy-register.component.ts
+++ src/app/components/pharmacy-register/pharmacy-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pharmacy-register',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PharmacyRegisterComponent implements OnInit {
 
-  constructor() { }
+  FormPharmacy: FormGroup;
+  submitted: boolean = false;
+
+  constructor(
+    public formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.FormPharmacy = this.formBuilder.group({
+      pharmacyName: ['',[Validators.required, Validators.maxLength(55)]],
+      branchOffices: ['',[Validators.required]],
+      ownerName: ['',[Validators.required, Validators.maxLength(55), Validators.pattern('^[a-zA-Z\u00C0-\u00FF \']*$')]],
+      ownerLastName: ['', [Validators.required, Validators.maxLength(55), Validators.pattern('^[a-zA-Z\u00C0-\u00FF \']*$')]],
+      phone: ['', Validators.required],
+      email: ['',[Validators.required, Validators.email]],
+      address: ['',[Validators.required, Validators.maxLength(55), Validators.pattern('^[a-zA-Z0-9\u00C0-\u00FF \']*$')]]
+    })
+  }
+
+  onSubmit( form: FormGroup ) {
+
   }
 
 }
