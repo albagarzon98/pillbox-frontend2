732a5c24f91acdea9115bf545ca09b326f834546
Levian <levianlastra@gmail.com>
2021-10-02 20:29:15 -0300

    Añadido el campo horario de atención, añadida la extensión de google maps para la dirección

--- src/app/components/pharmacy-add/pharmacy-add.component.ts
+++ src/app/components/pharmacy-add/pharmacy-add.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GoogleMap } from '@angular/google-maps';
 import { Router } from '@angular/router';
 import { Pharmacy } from 'src/app/models/pharmacy';
 import Swal from 'sweetalert2';
@@ -12,6 +13,17 @@ import { PharmacyService } from '../../services/pharmacy.service';
 })
 export class PharmacyAddComponent implements OnInit {
 
+    //Maps
+    @ViewChild('mapSearchField') searchField: ElementRef;
+    @ViewChild(GoogleMap) map: GoogleMap;
+    initialCoordinates = {
+      lat: -31.420211, 
+      lng: -64.188854
+    }
+    mapConfigurations = {
+      zoomControl: true
+    }
+  
   submitted: boolean = false;
   FormPharmacyAdd: FormGroup;
   
@@ -29,10 +41,37 @@ export class PharmacyAddComponent implements OnInit {
       // ownerLastName: ['', [Validators.required, Validators.maxLength(55), Validators.pattern('^[a-zA-Z\u00C0-\u00FF \']*$')]],
       phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]{10,11}')]],
       contactEmail: ['',[Validators.required, Validators.email]],
+      businessHours: ['', [Validators.required, Validators.maxLength(100)]],
       address: ['',[Validators.required, Validators.maxLength(55)]]
     })
 
     this.getPharmacyAdd();
+  }
+
+  ngAfterViewInit():void {
+    
+    const searchBox = new google.maps.places.SearchBox(
+      this.searchField.nativeElement,
+    );
+    
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+      if ( places.length === 0 ) {
+        return;
+      }
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach(place => {
+        if (!place.geometry || !place.geometry.location) {
+          return;
+        }
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      this.map.fitBounds(bounds);
+    });
   }
 
   getPharmacyAdd() {
