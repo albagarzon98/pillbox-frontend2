8d6d5c5222ce3b162e1acdea111eb69ee269bb5b
Levian <levianlastra@gmail.com>
2021-10-02 17:29:29 -0300

    Añadido el icono de info con el tooltip junto al label del campo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ReminderComponent } from './components/reminder/reminder.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PharmacyComponent } from './components/pharmacy/pharmacy.component';
 import { PharmacyRegisterComponent } from './components/pharmacy-register/pharmacy-register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PharmacyRequestsComponent } from './components/pharmacy-requests/pharmacy-requests.component';
+import { PharmacyAddComponent } from './components/pharmacy-add/pharmacy-add.component';
 import { InterceptorService } from './interceptors/interceptor.service';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -30,9 +33,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { IconModule } from '../app/icon.module';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { ProfileComponent } from './components/profile/profile.component';
-import { PharmacyRequestsComponent } from './components/pharmacy-requests/pharmacy-requests.component';
-import { PharmacyAddComponent } from './components/pharmacy-add/pharmacy-add.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -69,7 +70,8 @@ import { PharmacyAddComponent } from './components/pharmacy-add/pharmacy-add.com
     MatSelectModule,
     MatDividerModule,
     NgxMaskModule.forRoot(),
-    GoogleMapsModule
+    GoogleMapsModule,
+    MatTooltipModule
   ],
   providers: [
     {
