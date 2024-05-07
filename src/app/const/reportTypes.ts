c9dcc86dc4297be433f22315cd595e4d7f92acc4
levianlastra <llastra@andreani.com>
2024-05-07 18:51:24 -0300

    - se añaden los filtros y el manejo de período

--- src/app/const/reportTypes.ts
+++ src/app/const/reportTypes.ts
@@ -1,6 +1,20 @@
 export const reportTypes = {
     pharmacyRequestReport: {
         service: "pharmacyRequestService",
-        title: "Reporte de solicitudes de farmacia"
+        title: "Reporte de solicitudes de farmacia",
+        hasPeriodfilter: true,
+        filters: [
+            {
+                label: "Estado: ",
+                formGroupName: "requestStatus",
+                type: "select",
+                values: [
+                    { value: "todas", viewValue: "Todas" },
+                    { value: "aprobado", viewValue: "Pendientes" },
+                    { value: "rechazado", viewValue: "Aprobadas" },
+                    { value: "pendiente", viewValue: "Rechazadas" },
+                ]
+            }
+        ]
     }
 }
