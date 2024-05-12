e0624061c74c2db2854c0abd30191f1fe7727c98
levianlastra <llastra@andreani.com>
2024-05-11 21:16:53 -0300

    - se añaden os titulos de la tabla

--- src/app/const/reportTypes.ts
+++ src/app/const/reportTypes.ts
@@ -1,20 +1,30 @@
+import { PharmacyRequest } from "../models/pharmacy-request";
+
 export const reportTypes = {
     pharmacyRequestReport: {
         serviceFunction: "getPharmacyRequests",
         title: "Reporte de solicitudes de farmacia",
         hasPeriodfilter: true,
+        model: PharmacyRequest,
         filters: [
             {
                 label: "Estado: ",
                 formGroupName: "status",
                 type: "select",
                 values: [
-                    { value: "todas", viewValue: "Todas" },
                     { value: "aprobado", viewValue: "Aprobadas" },
                     { value: "rechazado", viewValue: "Rechazadas" },
                     { value: "pendiente", viewValue: "Pendientes" },
                 ]
             }
-        ]
+        ],
+        tableTitles: {
+            creationDate: "Fecha",
+            pharmacyName: "Nombre",
+            address: "Dirección",
+            phoneNumber: "Teléfono",
+            contactEmail: "Email",
+            branchesNumber: "Número de sucursales",
+        }
     }
 }
