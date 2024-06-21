0187867293be7b189ac4a26d8a6b431ff7468589
levianlastra <llastra@andreani.com>
2024-06-21 17:34:47 -0300

    - Add pie-chart component

---
+++ src/app/components/reports/charts/pie-chart/pie-chart.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent implements OnInit {
+  @Input() data: any[];
+  single = [];
+  view: [number, number] = [700, 300];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.single = this.data;
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
